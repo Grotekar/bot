0eddc12fc988da2ae76840b354bb7472d2aa49c4
Igor Petrenko <igor06.03.1995@gmail.com>
2020-08-03 01:01:54 +0300

    Создал первый коммит в репозитории
    В качестве подготовки к разработке:
     - Присоединил Composer для удобства работы;
     - Установил через Composer PHPUnit для
     тестирования;
     - Установил через Composer VKApi SDK PHP
     для удобства работы с VKApi
     (возможно, в ближайшее время будет
     деинсталлирован за ненадобностью и
     из-за возникших проблем в эксплуатации).
    
    В реализации:
     - Скрипт отправляет запрос к серверу,
     чтобы получить данные сессии;
     - В цикле скрипт получает данные о
     сообщениях, отправленных боту;
     - На основании полученных данных
     строится запрос-ответ (эхо сообщения
     пользователя).

---
+++ src/vk.php
@@ -0,0 +1,35 @@
+<?php
+    require_once __DIR__.'/../vendor/autoload.php';
+
+    $group_id = 197571527;
+    //$user_id  =  123871732;
+
+    /*$data = json_decode(file_get_contents('https://api.vk.com/method/messages.getLongPollServer?group_id='
+        . $group_id . '&need_pts=1&lp_version=5.65&v=5.101&access_token=' . $token));*/
+    $data = json_decode(file_get_contents('https://api.vk.com/method/groups.getLongPollSettings?group_id='
+        . $group_id . '&v=5.101&message_new=1&access_token=' . $token));
+
+    $data1 = json_decode(file_get_contents('https://api.vk.com/method/groups.getLongPollServer?group_id='
+        . $group_id . '&enabled=1&v=5.101&access_token=' . $token));
+    $key = $data1->{'response'}->{'key'};
+    $server = $data1->{'response'}->{'server'};
+    $ts = $data1->{'response'}->{'ts'};
+
+    while (true) {
+        $response = json_decode(file_get_contents("{$server}?act=a_check&key={$key}&ts={$ts}&wait=25"));
+        $object  = $response->{'updates'}[0]->{'object'};
+
+        $user_id  = $object->{'from_id'};
+        $random_id  = $object->{'random_id'};
+        $peer_id  = $object->{'peer_id'};
+        $message  = urlencode($object->{'text'});
+
+        $answer   = json_decode(file_get_contents('https://api.vk.com/method/messages.send?user_id='. $user_id . '&random_id=' . $random_id . '&peer_id='. $peer_id . '&message=' . $message . '&group_id=' . $group_id . '&v=5.101&access_token=' . $token));
+
+        $ts = $response->{'ts'};
+        var_dump($answer);
+        /*var_dump($object);
+        var_dump($user_id);*/
+    }
+
+    echo 'OK';
