6736f775d31292f7256f527bc07db3ffed4f4236
Igor Petrenko <igor06.03.1995@gmail.com>
2020-08-04 17:40:08 +0300

    Доработаны возможности ботов
    Для бота Vkontakte по команде /repeat появляется вопрос о количестве
    повторов и клавиатура выбора возле поля ввода.
    
    Для бота Telegram по команде /repeat также появляется вопрос о
    количестве повторов. Клавиатура выбора появляется возле самого
    сообщения. Это связано с особенностями реализации.

--- src/telegram.php
+++ src/telegram.php
@@ -9,37 +9,92 @@
     $update_id = -1;
 
     // Ожидание сообщения
-    while (true) {
-        // Получить возможное сообщение
-        $response = json_decode(file_get_contents('https://api.telegram.org/bot' . $token . '/getUpdates?offset='
-                 . $update_id));
-        // Если ответ существует
-        if (count($response->result) !== 0) {
-            // Анализировать полученные данные
-            $chat_id = $response->result[0]->message->chat->id;
-            $message = '';
+while (true) {
+    // Получить возможное сообщение
+    $response = json_decode(file_get_contents('https://api.telegram.org/bot' . $token . '/getUpdates?offset='
+             . $update_id));
+    // Если ответ существует и это обычное сообщение (не ответ на /repeat
+    if (count($response->result) !== 0 && array_key_exists('message', $response->result[0])) {
+        // Анализировать полученные данные
+        $chat_id = $response->result[0]->message->chat->id;
+        $message = '';
+        $keyboard = '';
 
-            // Анализ полученного сообщения
-            switch ($response->result[0]->message->text) {
-                case '/help':
-                    $message = urlencode($description);
-                    break;
-                case '/repeat':
-                    $message = urlencode('Сейчас повторяю ' . $repeat . " раз\n" . $question);
-                    break;
-                default:
-                    $message = urlencode($response->result[0]->message->text);
-                    break;
-            }
-            for ($i = 0; $i < $repeat; $i++) {
-                // Ответ
-                $data = json_decode(file_get_contents('https://api.telegram.org/bot' . $token . '/sendMessage?chat_id='
-                    . $chat_id . '&text=' . $message));
-            }
+        // Анализ полученного сообщения
+        switch ($response->result[0]->message->text) {
+            case '/help':
+                $message = urlencode($description);
+                break;
+            case '/repeat':
+                $message = urlencode('Сейчас повторяю ' . $repeat . " раз\n" . $question);
 
-            $update_id = $response->result[0]->update_id;
-            // Увеличить индекс события
-            $update_id++;
-            echo "OK\n";
+                $keyboard = json_encode(array(
+                    "inline_keyboard" => array(
+                        array(
+                            array(
+                                "text" => "1",
+                                "callback_data" => "button1"
+                            ),
+                            array(
+                                "text" => "2",
+                                "callback_data" => "button2"
+                            ),
+                            array(
+                                "text" => "3",
+                                "callback_data" => "button3"
+                            ),
+                            array(
+                                "text" => "4",
+                                "callback_data" => "button4"
+                            ),
+                            array(
+                                "text" => "5",
+                                "callback_data" => "button5"
+                            )
+                        )
+                    )
+                ));
+                break;
+            default:
+                $message = urlencode($response->result[0]->message->text);
+                break;
         }
+
+        for ($i = 0; $i < $repeat; $i++) {
+            // Ответ
+            // Если клавиатура есть
+            if ($keyboard === '') {
+                $data = json_decode(file_get_contents('https://api.telegram.org/bot' . $token
+                    . '/sendMessage?chat_id=' . $chat_id . '&text=' . $message));
+            } else { // достаточно спросить один раз
+                $data = json_decode(file_get_contents('https://api.telegram.org/bot' . $token
+                      . '/sendMessage?chat_id=' . $chat_id . '&text=' . $message . '&reply_markup=' . $keyboard));
+                break;
+            }
+        }
+
+        $update_id = $response->result[0]->update_id;
+        // Увеличить индекс события
+        $update_id++;
+        echo "OK\n";
+
+        // Иначе, если ответ на /repeat
+    } elseif (count($response->result) !== 0 && array_key_exists('callback_query', $response->result[0])) {
+        $button = $response->result[0]->callback_query->data;
+        $chat_id = $response->result[0]->callback_query->message->chat->id;
+        $repeat = (int) substr($button, -1);
+        $message = 'Количество повторений изменено на ' . $repeat;
+        $data = json_decode(file_get_contents('https://api.telegram.org/bot' . $token
+            . '/sendMessage?chat_id=' . $chat_id . '&text=' . $message));
+
+        $callback_query_id = $response->result[0]->callback_query->id;
+
+        $data1 = json_decode(file_get_contents('https://api.telegram.org/bot' . $token
+            . '/answerCallbackQuery?callback_query_id=' . $callback_query_id));
+        print_r($response);
+
+        $update_id = $response->result[0]->update_id;
+        $update_id++;
+        echo "OK\n";
     }
+}
--- src/vk.php
+++ src/vk.php
@@ -8,9 +8,6 @@
     $description = DESCRIPTION;
     $group_id    = 197571527;
 
-    /*$data = json_decode(file_get_contents('https://api.vk.com/method/groups.getLongPollSettings?group_id='
-        . $group_id . '&v=5.101&message_new=1&access_token=' . $token));*/
-
     // Запрос сессии
     $data  = json_decode(file_get_contents('https://api.vk.com/method/groups.getLongPollServer?group_id='
         . $group_id . '&enabled=1&v=5.101&access_token=' . $token));
@@ -22,13 +19,15 @@
         // Прослушивание и получение событий
         $response = json_decode(file_get_contents("{$server}?act=a_check&key={$key}&ts={$ts}"));
         // Если есть элементы
-        if (count($response->updates) !== 0) {
+        if (count($response->updates) !== 0 && array_key_exists('payload', $response->updates[0]->object) === false) {
+            //print_r($response->updates[0]);
             // Разбор полученного ответа
             $object = $response->updates[0]->object;
             $user_id = $object->from_id;
             $random_id = $object->random_id;
             $peer_id = $object->peer_id;
             $message = '';
+            $keyboard = '';
 
             // Анализ полученного сообщения
             switch ($object->text) {
@@ -37,36 +36,78 @@
                     break;
                 case '/repeat':
                     $message = urlencode('Сейчас повторяю ' . $repeat . " раз\n" . $question);
+                    $keyboard = json_encode(array(
+                        "one_time" => true,
+                        "buttons" => array(
+                            array(
+                                array(
+                                    "action" => array(
+                                        "type" => "text",
+                                        "payload" => '{"button":"1"}',
+                                        "label" => "1"
+                                    )
+                                ),
+                                array(
+                                    "action" => array(
+                                        "type" => "text",
+                                        "payload" => '{"button":"2"}',
+                                        "label" => "2"
+                                    )
+                                ),
+                                array(
+                                    "action" => array(
+                                        "type" => "text",
+                                        "payload" => '{"button":"3"}',
+                                        "label" => "3"
+                                    )
+                                ),
+                                array(
+                                    "action" => array(
+                                        "type" => "text",
+                                        "payload" => '{"button":"4"}',
+                                        "label" => "4"
+                                    )
+                                ),
+                                array(
+                                    "action" => array(
+                                        "type" => "text",
+                                        "payload" => '{"button":"5"}',
+                                        "label" => "5"
+                                    )
+                                ),
+                            )
+                        ),
+                        'inline' => false
+                    ));
                     break;
                 default:
                     $message = urlencode($object->text);
                     break;
             }
-            //var_dump($object->text);
-            /*$kbd  = [
-                "one_time" => false,
-                "buttons" => [
-                    [
-                        [
-                            "action" => [
-                                "type" => "text",
-                                "payload" => '{"button": "1"}',
-                                "label" => "Hello"
-                            ],
-                            "color" => "primary",
-                        ]
-                    ],
-                ],
-            ];
-            $keyboard = json_decode($kbd, JSON_UNESCAPED_UNICODE);*/
-            //var_dump($keyboard);
+
 
             // Вывод сообщения $repeat раз
             for ($i = 0; $i < $repeat; $i++) {
                 $answer = json_decode(file_get_contents('https://api.vk.com/method/messages.send?user_id='
                     . $user_id . '&random_id=' . $random_id . '&peer_id=' . $peer_id . '&message=' . $message
-                    . '&group_id=' . $group_id . '&v=5.101&access_token=' . $token));
+                    . '&group_id=' . $group_id . '&keyboard=' . $keyboard . '&v=5.101&access_token=' . $token));
+                //print_r($answer);
             }
+
+            // Обновление номера последнего события
+            $ts = $response->{'ts'};
+            echo "OK\n";
+
+        } elseif (count($response->updates) !== 0
+            && array_key_exists('payload', $response->updates[0]->object) === true) {
+            $button = json_decode($response->updates[0]->object->payload);
+            //print_r($button->button);
+            /*$chat_id = $response->result[0]->callback_query->message->chat->id;*/
+            $repeat = $button->button;
+            $message = urlencode('Количество повторений изменено на ' . $repeat);
+            $answer = json_decode(file_get_contents('https://api.vk.com/method/messages.send?user_id='
+                . $user_id . '&random_id=' . $random_id . '&peer_id=' . $peer_id . '&message=' . $message
+                . '&group_id=' . $group_id . '&v=5.101&access_token=' . $token));
 
             // Обновление номера последнего события
             $ts = $response->{'ts'};
