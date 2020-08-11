340a231410ab709ef1fd8fcd864874fbb9b4e5a8
Igor Petrenko <igor06.03.1995@gmail.com>
2020-08-11 20:56:54 +0300

    Изменены файлы config.php, composer.json
    В файл config.php добавлены переменные:
     - VK_GROUP_ID для задания идентификатора группы Вконтакте;
     - LOG_LEVEL для задания уровня логирования. Уровни можно посмотреть
     в реализации класса src/Utils/Logger.php.
    
    В класс src/Utils/Logger.php добавлена проверка на заданный уровень
    логирования. Теперь логироваться будут события только указанного
    уровня и ниже.
    
    В файл composer.json добавлена автозагрузка файла config.php. И файла
    тестирования tests/dataForTesting.php. Файл будет добавлен в
    репозиторий в следующем коммите.

--- config.php
+++ config.php
@@ -2,6 +2,8 @@
 
 define('VK_API_ACCESS_TOKEN', '123456');
 define('TELEGRAM_API_ACCESS_TOKEN', '123456');
+define('VK_GROUP_ID', 123456);
 define('QUESTION_FOR_REPEAT', 'Сколько раз повторить?');
-define('DESCRIPTION', 'Бот пока ничего не умеет. Только ваше эхо');
+define('DESCRIPTION', 'Бот - эхо ваших текстовых сообщений. Для изменения количества повторов введите команду /repeat');
 define('NUMBER_OF_REPETITIONS', 1);
+define('LOG_LEVEL', 8);
--- src/Utils/Logger.php
+++ src/Utils/Logger.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace Bot\Utils;
 
 use Psr\Log\AbstractLogger;
@@ -17,14 +16,45 @@ class Logger extends AbstractLogger
      */
     public function log($level, $message, array $context = array())
     {
-        // Построение массива подстановки с фигурными скобками
-        // вокруг значений ключей массива context.
-        $replace = array();
-        foreach ($context as $key => $val) {
-            $replace['{' . $key . '}'] = $val;
+        // Какой уровень логирования установлен
+        $levelToNumber = LOG_LEVEL;
+        switch ($level) {
+            case 'emergency':
+                $levelToNumber = 1;
+                break;
+            case 'alert':
+                $levelToNumber = 2;
+                break;
+            case 'critical':
+                $levelToNumber = 3;
+                break;
+            case 'error':
+                $levelToNumber = 4;
+                break;
+            case 'warning':
+                $levelToNumber = 5;
+                break;
+            case 'notice':
+                $levelToNumber = 6;
+                break;
+            case 'info':
+                $levelToNumber = 7;
+                break;
+            case 'debug':
+                $levelToNumber = 8;
+                break;
         }
 
-        // Подстановка значений в сообщение и возврат результата.
-        echo $level . ': ' . strtr($message, $replace) . "\n";
+        if ($levelToNumber <= LOG_LEVEL) {
+            // Построение массива подстановки с фигурными скобками
+            // вокруг значений ключей массива context.
+            $replace = array();
+            foreach ($context as $key => $val) {
+                $replace['{' . $key . '}'] = $val;
+            }
+
+            // Подстановка значений в сообщение и возврат результата.
+            echo $level . ': ' . strtr($message, $replace) . "\n";
+        }
     }
 }
