11cc47458f03225fc5d58c903c67239b44598976
Igor Petrenko <43473115+Grotekar@users.noreply.github.com>
2020-08-17 21:19:56 +0300

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('VK_API_ACCESS_TOKEN', '123456');
-define('TELEGRAM_API_ACCESS_TOKEN', '123456');
-define('VK_GROUP_ID', 123456);
-define('QUESTION_FOR_REPEAT', 'Сколько раз повторить?');
-define('DESCRIPTION', 'Бот - эхо ваших текстовых сообщений. Для изменения количества повторов введите команду /repeat');
-define('NUMBER_OF_REPETITIONS', 1);
-define('LOG_LEVEL', 8);
